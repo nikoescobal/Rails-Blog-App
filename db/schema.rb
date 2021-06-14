6d6b27b6468a1751b915a207fe92fe8022458086
Niko Escobal <niko.escobal@gmail.com>
2021-06-14 23:53:51 +0800

    Test console with data

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_152051) do
+ActiveRecord::Schema.define(version: 2021_06_14_154033) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "text"
+    t.string "image"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "priority"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -29,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_152051) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -41,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_152051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
