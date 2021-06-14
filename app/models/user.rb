bc77fe34dc931dafb931789ae68ea361f06dee98
Niko Escobal <niko.escobal@gmail.com>
2021-06-14 23:19:06 +0800

    Add vote and category models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  has_many :votes, through: :articles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
