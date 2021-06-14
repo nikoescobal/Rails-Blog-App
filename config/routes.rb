c9096c20c7d113f897c1d837d421280d8d0b1687
Niko Escobal <niko.escobal@gmail.com>
2021-06-14 23:15:21 +0800

    Configure devise for user model and add article model via resources generated command.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+  devise_for :users
+  # root to: "article#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210614150624_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
