be3c1812e11fbc972f8ddf7d46440c5da5830094
Niko Escobal <niko.escobal@gmail.com>
2021-06-15 22:25:55 +0800

    Create partials for form and updated models

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,11 @@ class Article < ApplicationRecord
   has_many :article_categories
   has_many :categories, through: :article_categories
   has_many :votes, dependent: :destroy
+
+  validates :title, :image, presence: true
+  validates :text, length: { minimum: 25 }
+  validates :image, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: 'must be a JPG or PNG image'
+  }
 end
