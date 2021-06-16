440867f36999d1700e05609b3ab3ba579e48d4aa
Niko Escobal <niko.escobal@gmail.com>
2021-06-16 23:48:18 +0800

    Install and configure Tailwind 2.0

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
