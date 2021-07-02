1a704b339ee12c2f1a018eedcdd3c91f8a45e4c2
Niko Escobal <niko.escobal@gmail.com>
2021-07-03 05:28:46 +0800

    Add Votes controller actions.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,27 @@
 class VotesController < ApplicationController
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = Vote.new(vote_params)
+
+    respond_to do |format|
+      if @vote.save
+        format.html { redirect_to @vote, notice: "Vote was successfully created." }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @vote.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @vote.destroy
+    respond_to do |format|
+      format.html { redirect_to votes_url, notice: "Vote was successfully deleted." }
+      format.json { head :no_content }
+    end
 end
