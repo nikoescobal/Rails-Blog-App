636fe9ac6377bbbb6f7468628c2d7ebc630700b0
Niko Escobal <niko.escobal@gmail.com>
2021-07-11 07:59:47 +0800

    Adjust image of imges in index page for articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,9 +3,9 @@ module ArticlesHelper
   def main_image(article, is_main)
     if article.main_image.attached?
       if is_main
-        image_tag article.main_image, class: 'h-full object-cover max-h-full h-96 w-full bg-no-repeat bg-center'
+        image_tag article.main_image, class: 'h-full object-cover max-h-full w-full bg-no-repeat bg-center'
       else
-        image_tag article.main_image, class: 'bg-cover bg-no-repeat bg-center md:w-full lg:w-full hover:scale-110'
+        image_tag article.main_image, class: 'bg-cover object-cover h-64 max-h-full bg-no-repeat bg-center md:w-full lg:w-full hover:scale-110'
       end
     else
       image_tag 'placeholder', class: 'h-64 w-full'
