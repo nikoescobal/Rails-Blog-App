3c18745bae98d0d5ce8638f35ea7be1c8038f11a
Niko Escobal <niko.escobal@gmail.com>
2021-07-17 19:01:58 +0800

    Add tests for rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Article, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   let(:user) { User.create(email: 'leevi@gmail.com', password: '123456', password_confirmation: '123456', username:"leevi") }
   let(:category) { Category.create(name: 'Drink', priority: 1) }
-  let(:article) { Article.create(user_id: user.id, title:"Mike likes big muscles", text: "Big muscles like Mike", category_ids:[category.id]) }
+  let(:article) { Article.create(user_id: user.id, title:"Mike likes big cookies", text: "Big muscles like Mike", category_ids:[category.id]) }
 
   it "is not valid with invalid attributes" do
     expect(Article.new).not_to be_valid
@@ -15,6 +15,24 @@ RSpec.describe Article, type: :model do
     expect(article).to be_valid
   end
 
-  
+  it 'is not valid without a title.' do
+    Article.new(title:'', user_id: user.id, text: "Big muscles like Mike", category_ids:[category.id])
+    expect(Article.new).not_to be_valid
+  end
 
+  it 'it not valid without a text' do
+    Article.new(title:"Mike likes big cookies", user_id: user.id, category_ids:[category.id])
+    expect(Article.new).not_to be_valid
+  end
+
+  it 'it not valid without a user' do
+    Article.new(title:"Mike likes big cookies", user_id: nil, title:"Mike likes big cookies", text: "Big muscles like Mike", category_ids:[category.id])
+    expect(Article.new).not_to be_valid
+  end
+
+  it 'it not valid without a category' do
+    Article.new(title:"Mike likes big cookies", user_id: user.id, title:"Mike likes big cookies", text: "Big muscles like Mike", category_ids:nil)
+    expect(Article.new).not_to be_valid
+  end
 end
+
