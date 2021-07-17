57985eff167ec31118375d403c4d4e91713eb454
Niko Escobal <niko.escobal@gmail.com>
2021-07-17 12:05:47 +0800

    Fix rspec tests error. Change user for testing.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,22 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  let(:user) { User.create(email: 'levi@gmail.com', password: '123456', password_confirmation: '123456', username:"levi") }
+  let(:user) { User.create(email: 'leevi@gmail.com', password: '123456', password_confirmation: '123456', username:"leevi") }
   let(:category) { Category.create(name: 'Drink', priority: 1) }
-  let(:article) { Article.create(user_id:1, title:"Mike likes big muscles", text: "Big muscles like Mike", category_ids:[category.id]) }
-
+  let(:article) { Article.create(user_id: user.id, title:"Mike likes big muscles", text: "Big muscles like Mike", category_ids:[category.id]) }
 
   it "is not valid with invalid attributes" do
     expect(Article.new).not_to be_valid
   end
 
-
-  # let(:article) { Article.create(user_id:1, title:"Mike likes big cookies", text: "Big cookies like Mike" ) }
   it "is valid with valid attributes" do
-    # main_image: File.open(File.join(Rails.root,'db','images','Event1.jpg'))
-    article.main_image.attach(io: File.open(Rails.root.join('./digitalnomad.jpeg')))
-    puts article.main_image
+    article.main_image.attach(io: File.open(Rails.root.join('spec', 'models', 'digitalnomad.jpeg')), filename: 'digitalnomad.jpeg', content_type: 'image/jpeg')
     expect(article).to be_valid
   end
+
+  
 
 end
