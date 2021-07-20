1a92265ef6dbf7b4ee3f69ca12c355c7afd317a9
Niko Escobal <niko.escobal@gmail.com>
2021-07-21 02:54:02 +0800

    Update tailwind config purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-    './app/**/.html.erb',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    enabled: false
+    // './app/**/.html.erb',
+    // './app/helpers/**/*.rb',
+    // './app/javascript/**/*.js'
   ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
